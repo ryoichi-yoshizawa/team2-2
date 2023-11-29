ec4255ffa0819e124dc8988e112468768a9f6473
ryoichi-yoshizawa <85666824+ryoichi-yoshizawa@users.noreply.github.com>
2023-11-29 20:59:17 +0900

    chatgpt

--- chatgpt/views.py
+++ chatgpt/views.py
@@ -1,3 +1,23 @@
+import openai
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+# Config/settings.pyからAPIキーを取得
+from django.conf import settings
+
+openai.api_key = settings.OPENAI_API_KEY
+
+@csrf_exempt  # CSRF保護を無効にする（開発用）
+def chat(request):
+    if request.method == 'POST':
+        user_input = request.POST.get('user_input')
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_input,
+            max_tokens=150
+        )
+        return JsonResponse({'response': response.choices[0].text.strip()})
+
+    # GETリクエストの場合
+    return render(request, 'chatgpt/chatgpt.html')
